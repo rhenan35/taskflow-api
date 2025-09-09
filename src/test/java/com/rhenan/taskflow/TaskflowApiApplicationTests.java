8c9098caefa0c1db3a97d86f2074e24e45717995
Rhenan Coelho <rhenan@recodeit.com.br>
2025-09-08 21:33:59 -0300

    chore: projeto inicial gerado

---
+++ src/test/java/com/rhenan/taskflow/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhenan.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
