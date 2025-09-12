043688f294482ca9ca69457dc2205cb920e21132
rhenan35 <52601467+rhenan35@users.noreply.github.com>
2025-09-11 23:33:43 -0300

    Revert "Feature/application (#10)" (#11)
    This reverts commit 27059b3f18dfb7bddd2d12d8d469042c0b055522.

--- src/main/java/com/rhenan/taskflow/domain/exception/BusinessRuleException.java
+++
@@ -1,7 +0,0 @@
-package com.rhenan.taskflow.domain.exception;
-
-public class BusinessRuleException extends RuntimeException {
-    public BusinessRuleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/rhenan/taskflow/domain/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rhenan.taskflow.domain.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
