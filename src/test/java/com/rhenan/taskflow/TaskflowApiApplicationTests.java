27917d1154f717510b82a2a0c968ec2981e563a6
rhenan35 <52601467+rhenan35@users.noreply.github.com>
2025-09-11 23:33:27 -0300

    Revert "Feature/application (#10)"
    This reverts commit 27059b3f18dfb7bddd2d12d8d469042c0b055522.

--- src/test/java/com/rhenan/taskflow/TaskflowApiApplicationTests.java
+++ src/test/java/com/rhenan/taskflow/TaskflowApiApplicationTests.java
@@ -2,10 +2,8 @@ package com.rhenan.taskflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class TaskflowApiApplicationTests {
 
 	@Test
